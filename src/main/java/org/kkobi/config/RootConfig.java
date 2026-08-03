65f02f119da40909760135c762aef2483b9b47b4
pz1u <jiu0414a@gmail.com>
2026-08-03 23:38:24 +0900

    #36 Feat : Spring Redis 연동 및 서버 연결 검증 (#37)

--- src/main/java/org/kkobi/config/RootConfig.java
+++ src/main/java/org/kkobi/config/RootConfig.java
@@ -8,10 +8,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
@@ -32,6 +29,7 @@ import javax.sql.DataSource;
         "org.kkobi.product.mapper",
         "org.kkobi.account.mapper"
 })
+@Import(RedisConfig.class)
 public class RootConfig {
     @Value("${jdbc.driver}") String driver;
     @Value("${jdbc.url}") String url;
