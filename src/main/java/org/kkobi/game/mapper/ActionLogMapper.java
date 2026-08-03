c6029a8b0fac1845ceca26b0bc3767fb0ee0f39e
jiny1027 <joojinyoung1027@gmail.com>
2026-08-03 09:41:19 +0900

    #21 Feat : 게임 행동 로그(ActionLog) DTO, Mapper, Service 구현 (#23)

---
+++ src/main/java/org/kkobi/game/mapper/ActionLogMapper.java
@@ -0,0 +1,15 @@
+package org.kkobi.game.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.kkobi.game.dto.ActionLogDto;
+
+import java.util.List;
+
+public interface ActionLogMapper {
+
+    // 게임 행동 로그 저장
+    int saveActionLog(ActionLogDto actionLog);
+
+    // 사용자별 게임 행동 로그 조회
+    List<ActionLogDto> getActionLogsByUserId(@Param("userId") Long userId);
+}
