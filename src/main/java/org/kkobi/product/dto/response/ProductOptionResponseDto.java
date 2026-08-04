21514704093a4f7a9e26e95a5c2386219543630c
pz1u <jiu0414a@gmail.com>
2026-08-04 10:21:28 +0900

    #38 Feat : 상품 상세 응답 DTO 추가

---
+++ src/main/java/org/kkobi/product/dto/response/ProductOptionResponseDto.java
@@ -0,0 +1,35 @@
+package org.kkobi.product.dto.response;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+// 상품 금리 옵션 조회 응답 데이터
+@Data
+public class ProductOptionResponseDto {
+
+    // 상품 옵션 식별자
+    private Long productOptionId;
+
+    // 금리 유형 코드
+    private String interestRateType;
+
+    // 금리 유형 이름
+    private String interestRateTypeName;
+
+    // 적립 유형 코드, 예금은 NONE
+    private String reserveType;
+
+    // 적립 유형 이름
+    private String reserverTypeName;
+
+    // 가입 기간
+    private Integer savingTerm;
+
+    // 기본 금리
+    private Integer interestRate;
+
+    // 최고 우대 금리
+    private BigDecimal maximumInterestRate;
+}
