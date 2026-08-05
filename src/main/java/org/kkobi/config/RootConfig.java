8b0abee340cfd852a31c4f422557d7639890263a
jiny1027 <joojinyoung1027@gmail.com>
2026-08-05 12:41:02 +0900

    #24 Fix : 게임 계산기 컴포넌트 스캔 등록

--- src/main/java/org/kkobi/config/RootConfig.java
+++ src/main/java/org/kkobi/config/RootConfig.java
@@ -20,6 +20,7 @@ import javax.sql.DataSource;
 @PropertySource({"classpath:/application.properties"})
 @ComponentScan(basePackages = {
         "org.kkobi.assessment",
+        "org.kkobi.game.calculator",
         "org.kkobi.game.service",
         "org.kkobi.product.deposit.service",
         "org.kkobi.product.saving.service",
