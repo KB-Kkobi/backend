caeb983141257fa1272940fe4182c53de9605846
pz1u <jiu0414a@gmail.com>
2026-08-09 23:39:41 +0900

    #73 Chore : Swagger OpenAPI 기본 환경 구성

--- src/main/java/org/kkobi/config/ServletConfig.java
+++ src/main/java/org/kkobi/config/ServletConfig.java
@@ -2,8 +2,16 @@ package org.kkobi.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springdoc.core.*;
+import org.springdoc.webmvc.core.MultipleOpenApiSupportConfiguration;
+import org.springdoc.webmvc.core.SpringDocWebMvcConfiguration;
+import org.springdoc.webmvc.ui.SwaggerConfig;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -18,6 +26,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 import java.util.List;
 
+@Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {
         "org.kkobi.controller",
@@ -30,6 +39,18 @@ import java.util.List;
         "org.kkobi.account.controller",
         "org.kkobi.external.kis.controller",
         "org.kkobi.external.kis.websocket"
+})
+@Import({
+        SpringDocConfigProperties.class,
+        SpringDocConfiguration.class,
+        SpringDocUIConfiguration.class,
+        SpringDocWebMvcConfiguration.class,
+        MultipleOpenApiSupportConfiguration.class,
+        SwaggerUiConfigProperties.class,
+        SwaggerUiOAuthProperties.class,
+        SwaggerConfig.class,
+        CacheOrGroupedOpenApiCondition.class,
+        JacksonAutoConfiguration.class
 })
 public class ServletConfig implements WebMvcConfigurer {
 
@@ -63,6 +84,7 @@ public class ServletConfig implements WebMvcConfigurer {
         ObjectMapper mapper = Jackson2ObjectMapperBuilder.json()
                 .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 .build();
+        converters.add(new ByteArrayHttpMessageConverter());
         converters.add(new MappingJackson2HttpMessageConverter(mapper));
     }
 }
