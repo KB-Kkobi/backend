000468afd4d6e9ef818aa10c62440a2b36b0f20b
pz1u <jiu0414a@gmail.com>
2026-08-10 10:47:53 +0900

    #73 Chore : Swagger API 명세 구성 완료

--- src/main/java/org/kkobi/controller/HealthController.java
+++ src/main/java/org/kkobi/controller/HealthController.java
@@ -1,5 +1,6 @@
 package org.kkobi.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,10 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@Tag(
+        name = "서버 상태 확인 API",
+        description = "백엔드 서버의 정상 동작 여부를 확인하는 API"
+)
 public class HealthController {
 
     // 애플리케이션 실행 상태를 반환
