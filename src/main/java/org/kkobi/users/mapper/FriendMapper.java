6b68a323a3c448a1f758de74b09627382a55dbe3
pz1u <jiu0414a@gmail.com>
2026-08-12 18:51:08 +0900

    #107 Feat : 친구 관리 기능 구현 (#118)

---
+++ src/main/java/org/kkobi/users/mapper/FriendMapper.java
@@ -0,0 +1,47 @@
+package org.kkobi.users.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import org.kkobi.users.dto.response.FriendRequestResponseDto;
+import org.kkobi.users.dto.response.FriendResponseDto;
+import org.kkobi.users.enums.FriendshipStatus;
+import org.springframework.security.core.parameters.P;
+
+import java.util.List;
+
+public interface FriendMapper {
+
+    // 두 사용자 사이에 친구 관계 또는 요청이 존재하는지 확인
+    int countRelationship(
+            @Param("userId") Long userId,
+            @Param("targetUserId") Long targetUserId
+    );
+
+    // 친구 요청을 저장
+    int insertFriendRequest(
+            @Param("requesterId") Long requesterId,
+            @Param("receiverId") Long receiverId,
+            @Param("status") FriendshipStatus status
+            );
+
+    // 받은 친구 요청 목록을 조회
+    List<FriendRequestResponseDto> findReceivedFriendRequests(
+            @Param("userId") Long userId
+    );
+
+    // 받은 친구 요청을 수락
+    int acceptFriendRequest(
+            @Param("friendshipId") Long friendshipId,
+            @Param("receiverId") Long receiverId
+    );
+
+    // 받은 친구 요청을 거절
+    int rejectFriendRequest(
+            @Param("friendshipId") Long friendshipId,
+            @Param("receiverId") Long receiverId
+    );
+
+    // 친구 목록을 조회
+    List<FriendResponseDto> findFriends(
+            @Param("userId") Long userId
+    );
+}
---
+++ src/main/java/org/kkobi/users/service/FriendServiceImpl.java
@@ -0,0 +1,97 @@
+package org.kkobi.users.service;
+
+import lombok.RequiredArgsConstructor;
+import org.kkobi.users.domain.UserVO;
+import org.kkobi.users.dto.request.FriendRequestDto;
+import org.kkobi.users.dto.response.FriendRequestResponseDto;
+import org.kkobi.users.dto.response.FriendResponseDto;
+import org.kkobi.users.enums.FriendshipStatus;
+import org.kkobi.users.mapper.FriendMapper;
+import org.kkobi.users.mapper.UserMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FriendServiceImpl implements FriendService{
+
+    private final FriendMapper friendMapper;
+    private final UserMapper userMapper;
+
+    // 닉네임으로 친구 요청을 전송
+
+    @Override
+    @Transactional
+    public void sendFriendRequest(Long userId, FriendRequestDto request) {
+
+        UserVO targetUser = userMapper.findByNickname(request.getNickname());
+
+        if(targetUser == null){
+            throw new IllegalArgumentException("존재하지 않는 닉네임입니다.");
+        }
+
+        if(userId.equals(targetUser.getUserId())) {
+            throw new IllegalArgumentException("자기 자신에게 친구 요청을 보낼 수 없습니다.");
+        }
+
+        if(friendMapper.countRelationship(userId, targetUser.getUserId()) > 0){
+            throw new IllegalArgumentException("이미 친구 관계 또는 친구 요청이 존재합니다.");
+        }
+
+        int insertRows = friendMapper.insertFriendRequest(
+                userId,
+                targetUser.getUserId(),
+                FriendshipStatus.PENDING
+        );
+
+        if(insertRows != 1){
+            throw new IllegalArgumentException("친구 요청 저장에 실패했습니다.");
+        }
+    }
+
+    // 받은 친구 요청 목록을 조회
+    @Override
+    @Transactional(readOnly = true)
+    public List<FriendRequestResponseDto> getReceiverFriendRequests(Long userId) {
+        return friendMapper.findReceivedFriendRequests(userId);
+    }
+
+    // 받은 친구 요청을 수락
+    @Override
+    @Transactional
+    public void acceptFriendRequest(Long userId, Long friendshipId) {
+
+        int updateRows = friendMapper.acceptFriendRequest(
+                friendshipId,
+                userId
+        );
+
+        if (updateRows != 1) {
+            throw new IllegalArgumentException("처리할 수 없는 친구 요청입니다.");
+        }
+    }
+
+    // 받은 친구 요청을 거절
+    @Override
+    @Transactional
+    public void rejectFriendRequest(Long userId, Long friendshipId) {
+
+        int deletedRows = friendMapper.rejectFriendRequest(
+                friendshipId,
+                userId
+        );
+
+        if(deletedRows != 1){
+            throw new IllegalArgumentException("처리할 수 없는 친구 요청입니다.");
+        }
+    }
+
+    // 친구 목록을 조회
+    @Override
+    @Transactional(readOnly = true)
+    public List<FriendResponseDto> getFriend(Long userId) {
+        return friendMapper.findFriends(userId);
+    }
+}
