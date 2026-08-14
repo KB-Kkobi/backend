cccf0c2297d46438c706024d3daa4eeda51fd28d
pz1u <jiu0414a@gmail.com>
2026-08-15 01:37:13 +0900

    #141 Feat : 우대조건 금리 파싱 및 자동 적재 구현

--- src/main/java/org/kkobi/product/parser/dto/ParsedPreferentialRateCondition.java
+++ src/main/java/org/kkobi/product/parser/dto/ParsedPreferentialRateCondition.java
@@ -1,4 +1,24 @@
 package org.kkobi.product.parser.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.kkobi.product.enums.PreferentialConditionType;
+
+import java.math.BigDecimal;
+
+@Getter
+@AllArgsConstructor
 public class ParsedPreferentialRateCondition {
+
+    // 우대조건 유형
+    private PreferentialConditionType conditionType;
+
+    // 사용자에게 표시할 우대조건 내용
+    private String conditionName;
+
+    // 조건 충족 시 추가되는 우대금리
+    private BigDecimal additionalRate;
+
+    // 사용자가 직접 선택할 수 있는 조건인지 여부
+    private boolean selectable;
 }
