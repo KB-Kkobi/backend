7a4c579039de68086c9fc8605cf1006c3a858634
JiunKim <jiunkim0921@gmail.com>
2026-08-18 14:53:44 +0900

    #166 Feat : 홈 화면 추천 종목·예금·적금 API 구성 변경 (#167)

--- src/main/java/org/kkobi/securities/dto/response/SecurityRecommendationResponse.java
+++ src/main/java/org/kkobi/securities/dto/response/SecurityRecommendationResponse.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 import java.util.List;
 
-// 홈 화면 추천 종목 응답 (주식 1 + 주식형 ETF 1 + 채권형 ETF 1)
+// 홈 화면 추천 종목 응답 (주식·주식형 ETF 통합 1 + 채권형 ETF 1)
 @Data
 public class SecurityRecommendationResponse {
 
