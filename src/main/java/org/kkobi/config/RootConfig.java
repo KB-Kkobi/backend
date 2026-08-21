4a94d96f1ce81758831ae2ad80c871e4b67e74a4
pz1u <jiu0414a@gmail.com>
2026-08-21 14:14:35 +0900

    현종이형 화이팅 (#180)

--- src/main/java/org/kkobi/config/RootConfig.java
+++ src/main/java/org/kkobi/config/RootConfig.java
@@ -61,7 +61,7 @@ import javax.sql.DataSource;
         "org.kkobi.trade.mapper",
         "org.kkobi.leaderboard.mapper",
         "org.kkobi.quiz.mapper",
-        "org.kkobi.users.mapper"
+        "org.kkobi.users.mapper",
         "org.kkobi.notification.mapper",
         "org.kkobi.goal.mapper",
 })
